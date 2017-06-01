421f8198a3e175a845bf48ba9a8ffc9e4e737f10
Ram-SR <rammurthythati@gmail.com>
2017-06-01 16:44:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function newKeyword(args, functionName) {
-   var object = {}; // create brand new object
-   functionName.call(object, args); // call the function with passed arguments in the scope of object
-   return object; // return object into variable
+/*******************************************************************************************/
+*  version v0.0                                                                             *
+*  author: Rammurthy                                                                        *
+*  reference: Mozilla Developer Network                                                     *
+*  ref_link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/new
+*********************************************************************************************/
+
+function newKeyword() { 
+   if (arguments.length) { 
+      var args = arguments; 
+      var functionName = arguments[arguments.length - 1]; 
+      args = [].pop.call(arguments); 
+      var object = {}; // create brand new object 
+      functionName.apply(object, args); // call the function with passed arguments in the scope of object 
+      return object; // return object into variable 
+   } else { //do nothing 
+   } 
 }
