3ebf75e7c9387d58a9a3df0c68ccf6b853105f29
Ram-SR <rammurthythati@gmail.com>
2017-06-01 16:53:47 +0530

    Update new-keyword-equivalent.js

--- new-keyword-equivalent.js
+++ new-keyword-equivalent.js
@@ -1,1 +1,18 @@
+/*******************************************************************************************/
+*  version v0.0                                                                             *
+*  author: Rammurthy                                                                        *
+*  reference: Mozilla Developer Network                                                     *
+*  ref_link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/new
+*********************************************************************************************/
 
+function newKeyword() { 
+   if (arguments.length) { 
+      var args = arguments; 
+      var functionName = arguments[arguments.length - 1]; 
+      args = [].pop.call(arguments); 
+      var object = {}; // create brand new object 
+      functionName.apply(object, args); // call the function with passed arguments in the scope of object 
+      return object; // return object into variable 
+   } else { //do nothing 
+   } 
+}
